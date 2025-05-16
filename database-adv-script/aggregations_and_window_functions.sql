77068af79fcbb6d8a69b43699e8fec10ff26e137
Brenda Bonareri <brendabjb24@gmail.com>
2025-05-16 14:48:05 +0300

    Correction

--- database-adv-script/aggregations_and_window_functions.sql
+++ database-adv-script/aggregations_and_window_functions.sql
@@ -13,9 +13,10 @@ SELECT
     p.id AS property_id,
     p.name AS property_name,
     COUNT(b.id) AS total_bookings,
-    ROW_NUMBER() OVER (ORDER BY COUNT(b.id) DESC) AS rank
+    RANK() OVER (ORDER BY COUNT(b.id) DESC) AS rank
 FROM properties p
 LEFT JOIN bookings b ON p.id = b.property_id
 GROUP BY p.id, p.name;
+
 
 
