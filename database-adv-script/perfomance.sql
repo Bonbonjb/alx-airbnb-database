7bdd01b9d100ac0125eae9a3ddc508c790bf7e7b
Brenda Bonareri <brendabjb24@gmail.com>
2025-05-23 12:33:02 +0300

    performance.sql

---
+++ database-adv-script/perfomance.sql
@@ -0,0 +1,20 @@
+-- Initial unoptimized query to get all bookings with user, property, and payment details
+
+SELECT 
+    b.id AS booking_id,
+    u.id AS user_id,
+    u.name AS user_name,
+    p.id AS property_id,
+    p.name AS property_name,
+    pay.id AS payment_id,
+    pay.amount,
+    pay.status
+FROM 
+    bookings b
+JOIN 
+    users u ON b.user_id = u.id
+JOIN 
+    properties p ON b.property_id = p.id
+JOIN 
+    payments pay ON b.id = pay.booking_id;
+
